845a53d22ef1fa9eebb12db0ff9d4ff206b0fd26
Miroslav Petrov <m.petrov@7mojos.com>
2022-02-03 11:11:04 +0200

    Lesson 4 - Exporting Pixi App, Rendering config

--- 01/src/scripts/App.js
+++ 01/src/scripts/App.js
@@ -2,7 +2,7 @@ import * as PIXI from "pixi.js";
 
 export class App {
     run() {
-        this.app = new PIXI.Application({resizeTo: window});
+        this.app = new PIXI.Application({ resizeTo: window });
         console.log(this.app);
 
         document.body.appendChild(this.app.view);
