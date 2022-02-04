4bd41f601acb3aa6000bf7a8abab84adfe9c0198
Miroslav Petrov <m.petrov@7mojos.com>
2022-02-04 10:33:00 +0200

    Lesson 11 - Interactivity, Puzzle pieces movement

--- 01/src/scripts/PuzzlePiece.js
+++ 01/src/scripts/PuzzlePiece.js
@@ -4,9 +4,45 @@ import { Globals } from "./Globals";
 export class PuzzlePiece {
     constructor(id, field) {
         this.sprite = new PIXI.Sprite(Globals.resources[`puzzle${id}`].texture);
+
         this.sprite.x = field.x;
         this.sprite.y = field.y;
+
         this.sprite.anchor.set(0.5);
         this.sprite.scale.set(0.5);
+
+        this.field = field;
+
+        this.setInteractivity();
+    }
+
+    setInteractivity() {
+        this.sprite.interactive = true;
+        this.sprite.on("pointerdown", this.onTouchStart, this);
+        this.sprite.on("pointermove", this.onTouchMove, this);
+        this.sprite.on("pointerup", this.onRelease, this);
+    }
+
+    onTouchStart(event) {
+        this.touchPostion = { x: event.data.global.x, y: event.data.global.y };
+        this.dragging = true;
+    }
+
+    onTouchMove(event) {
+        if (!this.dragging) {
+            return;
+        }
+
+        const currentPosition = { x: event.data.global.x, y: event.data.global.y }
+
+        const offsetX = currentPosition.x - this.touchPostion.x;
+        const offsetY = currentPosition.y - this.touchPostion.y;
+
+        this.sprite.x = this.field.x + offsetX;
+        this.sprite.y = this.field.y + offsetY;
+    }
+
+    onRelease() {
+        this.dragging = false;
     }
 }
