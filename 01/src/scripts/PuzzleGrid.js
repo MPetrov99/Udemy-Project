808d2568535a3f68c188bbe6ee91449d2e7e8e43
Miroslav Petrov <m.petrov@7mojos.com>
2022-02-04 10:00:28 +0200

    Lesson 10 - Puzzle pieces at random spots on start

--- 01/src/scripts/PuzzleGrid.js
+++ 01/src/scripts/PuzzleGrid.js
@@ -13,8 +13,15 @@ export class PuzzleGrid {
     createPuzzlePieces() {
         this.pieces = [];
 
+
+        let ids = PuzzleGridConfig.map(field => field.id)
+
         PuzzleGridConfig.forEach(field => {
-            const piece = new PuzzlePiece(field.id, field);
+            const random = Math.floor(Math.random() * ids.length);
+            const id = ids[random];
+            ids = ids.filter(item => item !== id);
+
+            const piece = new PuzzlePiece(id, field);
             this.container.addChild(piece.sprite);
             this.pieces.push(piece);
         });
